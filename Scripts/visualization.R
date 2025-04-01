159f5fcdf958ea5c2270aa02862bf56d7f9a9f1e
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 01:31:07 -0400

    Update visualization.R

--- Scripts/visualization.R
+++ Scripts/visualization.R
@@ -1,8 +1,6 @@
 # Step 7: Visualizing and interpreting results in R
-# Import the ggplot2 library for plotting
 library(ggplot2)
 
-# Read the annotated variant data
 variants <- read.table("b1_annotated.hg19_multianno.txt", header=TRUE, sep="\t")
 
 # Generate a bar plot to visualize variant distribution by functional category
